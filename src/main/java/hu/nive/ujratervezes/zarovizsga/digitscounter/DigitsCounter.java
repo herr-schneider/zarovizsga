8d33fffc6a2654101b139fde2a8c28454ac38712
szabo ferenc <szaboferenc.kromat@gmail.com>
2021-03-16 17:35:00 +0100

    1. OK

--- src/main/java/hu/nive/ujratervezes/zarovizsga/digitscounter/DigitsCounter.java
+++ src/main/java/hu/nive/ujratervezes/zarovizsga/digitscounter/DigitsCounter.java
@@ -1,6 +1,22 @@
 package hu.nive.ujratervezes.zarovizsga.digitscounter;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class DigitsCounter {
 
+    public int getCountOfDigits(String s) {
+        if (s == null || s.length() < 1) {
+            return 0;
+        }
+        Set<Character> temp = new HashSet<>();
+        char[] chars = s.toCharArray();
+        for (char c : chars) {
+            if (c > 47 && c < 58) {
+                temp.add(c);
+            }
+        }
+        return temp.size();
+    }
 
 }
